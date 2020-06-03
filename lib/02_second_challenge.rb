97ba3e68040703fce671a06b85626b71f75fae80
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-03 13:26:18 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,14 +1,20 @@
-epic_tragedy = {
-   :montague => {
+def second_challenge
+  epic_tragedy = {
+    :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => []
-   },
-   :capulet => {
+      :hero_friends => 
+      }, 
+    :capulet => {
       :patriarch => {},
       :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
-   }
-}
+      :heroin => {},
+      :heroin_friends =>
+      
+      }
+    
+  }
+
+  
+end
